9219c97828655df55225da35d04e8c1a0a3ad04a
Abel Vieira <abel.5starcloud@gmail.com>
2026-05-28 19:24:58 -0300

    Coloquei funcoes sleeps pra dar mais fluides à leitura!

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@ set -x
 
 clear
 echo "Digite nome do arquivo"
+sleep 2
 read arquivo
 touch "$arquivo.txt"
 
@@ -12,27 +13,36 @@ clear
 
 while true; do
     echo -e "Arquivo em uso: $arquivo.txt\n"
+sleep 3
     echo "1 para adicionar texto no arquivo"
+ sleep 3
     echo "2 para sobrescrever texto"
+sleep 3
     echo "3 ler o arquivo"
+sleep 2
     echo "Digite a opção desejada"
     read opcao
 
 case $opcao in
     1) echo "Digite oq será adicionado no arquivo"
+    sleep 3
         read texto_ad
         echo "$texto_ad" >> "$arquivo.txt"
+    sleep 3
         echo "Texto adicionado!"
         sleep 2
         clear ;;
 
     2) echo "Tem CERTEZA que deseja sobrescrever tudo que existe?"
+    sleep 3
         echo "Digite S para sim e N para não"
+    
         read  SN
         if [ "$SN" == "S"] || [ "$SN" == "s" ]; then
             echo "Digite o texto que sobrescreverá tudo:"
             read texto_sob
             echo "$texto_sob" > "$arquivo.txt"
+   sleep 2
             echo "Texto sobrescrito com sucesso"
             sleep 2
             clear 
