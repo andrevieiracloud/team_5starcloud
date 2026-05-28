4df82c9d156bbaaaf2828dedeed3a48157b71fae
Rodrigo5starcloud <rodrigo5starcloud@gmail.com>
2026-05-28 14:37:46 -0300

    Remove clear para manter histórico visível

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -x
 
-clear
+
 echo "Digite nome do arquivo"
 read arquivo
 touch "$arquivo.txt"
 
 echo "Agora vamos editar o arquivo"
 sleep 2
-clear
+
 
 while true; do
     echo -e "Arquivo em uso: $arquivo.txt\n"
@@ -24,7 +24,7 @@ case $opcao in
         echo "$texto_ad" >> "$arquivo.txt"
         echo "Texto adicionado!"
         sleep 2
-        clear ;;
+         ;;
 
     2) echo "Tem CERTEZA que deseja sobrescrever tudo que existe?"
         echo "Digite S para sim e N para não"
@@ -35,10 +35,10 @@ case $opcao in
             echo "$texto_sob" > "$arquivo.txt"
             echo "Texto sobrescrito com sucesso"
             sleep 2
-            clear 
+             
         else
             echo "Você digitou nada"
-            clear
+            
         fi  ;;
 
     3) echo "Vamos ler o arquivo"
@@ -46,7 +46,7 @@ case $opcao in
         sleep 2
         cat "$arquivo.txt"
         read nada
-        clear ;;
+         ;;
 
 esac
 done
